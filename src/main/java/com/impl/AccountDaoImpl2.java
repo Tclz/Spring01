0b6037aecfa38800c92f453c3d3897594bd331f7
Tclz <1799873484@qq.com>
2020-05-26 01:36:22 +0800

    Update AccountDaoImpl2.java

--- src/main/java/com/impl/AccountDaoImpl2.java
+++ src/main/java/com/impl/AccountDaoImpl2.java
@@ -3,7 +3,7 @@ package com.impl;
 import com.dao.IAccountDao;
 import org.springframework.stereotype.Repository;
 
-//  与要注入的变量类型相同的bean有多个，AutoWired不工作
+//  与要注入的变量类型相同的bean有多个，AutoWired的表现
 @Repository(value = "accountDao2")
 
 public class AccountDaoImpl2 implements IAccountDao {
